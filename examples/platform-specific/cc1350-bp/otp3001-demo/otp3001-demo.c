4b041220de82117a69b74efc57e35986eb2b4764
Dennis Campagna <denniscampagna@gmail.com>
2018-07-10 19:01:49 +0200

    Created new board for CC1350 with sensor boosterpack with relative examples

---
+++ examples/platform-specific/cc1350-bp/otp3001-demo/otp3001-demo.c
@@ -0,0 +1,197 @@
+/*
+ * Copyright (c) 2014, Texas Instruments Incorporated - http://www.ti.com/
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. Neither the name of the copyright holder nor the names of its
+ *    contributors may be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE
+ * COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+ * OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+/*---------------------------------------------------------------------------*/
+/**
+ * \addtogroup cc26xx-platforms
+ * @{
+ *
+ * \defgroup cc26xx-examples CC26xx Example Projects
+ *
+ * Example projects for CC26xx-based platforms.
+ * @{
+ *
+ * \defgroup cc26xx-demo CC26xx Demo Project
+ *
+ *   Example project demonstrating the CC13xx/CC26xx platforms
+ *
+ *   This example will work for the following boards:
+ *   - srf06-cc26xx: SmartRF06EB + CC13xx/CC26xx EM
+ *   - CC2650 and CC1350 SensorTag
+ *   - CC1310, CC1350, CC2650 LaunchPads
+ *
+ *   This is an IPv6/RPL-enabled example. Thus, if you have a border router in
+ *   your installation (same RDC layer, same PAN ID and RF channel), you should
+ *   be able to ping6 this demo node.
+ *
+ *   This example also demonstrates CC26xx BLE operation. The process starts
+ *   the BLE beacon daemon (implemented in the RF driver). The daemon will
+ *   send out a BLE beacon periodically. Use any BLE-enabled application (e.g.
+ *   LightBlue on OS X or the TI BLE Multitool smartphone app) and after a few
+ *   seconds the cc26xx device will be discovered.
+ *
+ * - etimer/clock : Every CC26XX_DEMO_LOOP_INTERVAL clock ticks the LED defined
+ *                  as CC26XX_DEMO_LEDS_PERIODIC will toggle and the device
+ *                  will print out readings from some supported sensors
+ * - sensors      : Some sensortag sensors are read asynchronously (see sensor
+ *                  documentation). For those, this example will print out
+ *                  readings in a staggered fashion at a random interval
+ * - Buttons      : CC26XX_DEMO_TRIGGER_1 button will toggle CC26XX_DEMO_LEDS_BUTTON
+ *                - CC26XX_DEMO_TRIGGER_2 turns on LEDS_REBOOT and causes a
+ *                  watchdog reboot
+ *                - The remaining buttons will just print something
+ *                - The example also shows how to retrieve the duration of a
+ *                  button press (in ticks). The driver will generate a
+ *                  sensors_changed event upon button release
+ * - Reed Relay   : Will toggle the sensortag buzzer on/off
+ *
+ * @{
+ *
+ * \file
+ *     Example demonstrating the cc26xx platforms
+ */
+#include "contiki.h"
+#include "sys/etimer.h"
+#include "sys/ctimer.h"
+#include "dev/leds.h"
+#include "dev/watchdog.h"
+#include "dev/button-hal.h"
+#include "random.h"
+#include "button-sensor.h"
+#include "batmon-sensor.h"
+#include "board-peripherals.h"
+#include "rf-core/rf-ble.h"
+
+#include "ti-lib.h"
+
+#include <stdio.h>
+#include <stdint.h>
+/*---------------------------------------------------------------------------*/
+#define CC26XX_DEMO_LOOP_INTERVAL       (CLOCK_SECOND * 20)
+#define CC26XX_DEMO_LEDS_PERIODIC       LEDS_YELLOW
+#define CC26XX_DEMO_LEDS_BUTTON         LEDS_RED
+#define CC26XX_DEMO_LEDS_REBOOT         LEDS_ALL
+/*---------------------------------------------------------------------------*/
+#define CC26XX_DEMO_TRIGGER_1     BOARD_BUTTON_HAL_INDEX_KEY_LEFT
+#define CC26XX_DEMO_TRIGGER_2     BOARD_BUTTON_HAL_INDEX_KEY_RIGHT
+/*
+#if BOARD_SENSORTAG
+#define CC26XX_DEMO_TRIGGER_3     BOARD_BUTTON_HAL_INDEX_REED_RELAY
+#endif*/
+/*---------------------------------------------------------------------------*/
+static struct etimer et;
+/*---------------------------------------------------------------------------*/
+PROCESS(cc26xx_demo_process, "cc26xx demo process");
+AUTOSTART_PROCESSES(&cc26xx_demo_process);
+/*---------------------------------------------------------------------------*/
+//#if BOARD_SENSORTAG
+/*---------------------------------------------------------------------------*/
+/*
+ * Update sensor readings in a staggered fashion every SENSOR_READING_PERIOD
+ * ticks + a random interval between 0 and SENSOR_READING_RANDOM ticks
+ */
+#define SENSOR_READING_PERIOD (CLOCK_SECOND * 20)
+#define SENSOR_READING_RANDOM (CLOCK_SECOND << 4)
+
+static void
+get_tmp_reading()
+{
+    int value;
+
+    value = tmp_007_sensor.value(TMP_007_SENSOR_TYPE_ALL);
+
+    if(value == CC26XX_SENSOR_READING_ERROR) {
+        printf("TMP: Ambient Read Error\n");
+        return;
+    }
+
+    value = tmp_007_sensor.value(TMP_007_SENSOR_TYPE_AMBIENT);
+    printf("oltre il return sensor reading\n");
+    printf("TMP: Ambient=%d.%03d C\n", value / 1000, value % 1000);
+
+    value = tmp_007_sensor.value(TMP_007_SENSOR_TYPE_OBJECT);
+    printf("TMP: Object=%d.%03d C\n", value / 1000, value % 1000);
+}
+/*---------------------------------------------------------------------------*/
+static void
+get_light_reading()
+{
+    int value;
+    value = opt_3001_sensor.value(0);
+    if(value != CC26XX_SENSOR_READING_ERROR) {
+        printf("OPT: Light=%d.%02d lux\n", value / 100, value % 100);
+    } else {
+        printf("OPT: Light Read Error\n");
+    }
+
+    /* The OPT will turn itself off, so we don't need to call its DEACTIVATE */
+    //ctimer_set(&opt_timer, next, init_opt_reading, NULL);
+}
+
+static void
+init_sensor_readings(void)
+{
+    SENSORS_ACTIVATE(tmp_007_sensor);
+    SENSORS_ACTIVATE(opt_3001_sensor);
+}
+/*---------------------------------------------------------------------------*/
+PROCESS_THREAD(cc26xx_demo_process, ev, data)
+{
+
+PROCESS_BEGIN();
+
+printf("CC26XX demo\n");
+
+//init_sensors();
+
+/* Init the BLE advertisement daemon */
+rf_ble_beacond_config(0, BOARD_STRING);
+rf_ble_beacond_start();
+
+etimer_set(&et, CC26XX_DEMO_LOOP_INTERVAL);
+//get_sync_sensor_readings();
+init_sensor_readings();
+
+while(1) {
+SENSORS_ACTIVATE(opt_3001_sensor);//l'opt va da solo in pausa e non si risveglia pi� (non aggoirna pi� i valori per qusto le reinizializzo
+//ogni volta bisogna lasciare un po' di tempo tra l'ACTIVE e la lettura cos� si riesce a leggere senza errori
+PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&et));
+etimer_reset(&et);
+get_light_reading();
+get_tmp_reading();
+}
+
+PROCESS_END();
+}
+/*---------------------------------------------------------------------------*/
+/**
+ * @}
+ * @}
+ * @}
+ */
